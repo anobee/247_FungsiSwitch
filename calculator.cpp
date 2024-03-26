5559842dab4d002bb45877c57c8450a84f57060e
anobee <HP@DESKTOP-KFAVCD8>
2024-03-26 14:52:04 +0700

    1

---
+++ calculator.cpp
@@ -0,0 +1,54 @@
+#include<iostream>;
+using namespace std;
+
+int bilangan1, bilangan2;
+void inputData ()
+{
+    cout << "masukan bilangan pertama :";
+    cin >> bilangan1;
+    cout << "masukan bilangan kedua";
+    cin >> bilangan2;
+}
+
+
+int penjumlahan (int a, int b)
+{
+    return a + b
+}
+
+int pengurangan (int a, int b)
+{
+    return a - b
+}
+
+int perkalian (int a, int b)
+{
+    return a * b
+}
+
+int pembagian (int a, int b)
+{
+    return a / b
+}
+
+int main()
+
+{ 
+    int pilihan ;
+    do {
+        cout << "kalkulator sederhana" << endl;
+        cout << "====================" << endl;
+        cout << "1. penjumlahan" << endl;
+        cout << "2. pengurangan" << endl;
+        cout << "3. perkalian" << endl;
+        cout << "4. pembagian" << endl;
+        cout << "=============" << endl;
+        cout << "masukan menu pilihan :";
+        cin >> pilihan
+        
+        switch (pilihan)
+        
+
+
+    }
+}
